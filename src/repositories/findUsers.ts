f2d88dda7f027522b89b1a9765f8e54f7c103eee
Ricardo <ricardo.santos@docly.com.br>
2022-03-16 20:31:54 -0300

    feat(Global): refactor for class

--- src/repositories/findUsers.ts
+++ src/repositories/findUsers.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import IUser from '@interfaces/IUser';
 
-const findUsers = async (): Promise<IUser[]> => {
-    const baseUrl = process.env.DOCLY_URL;
-    const response = await axios.get(`${baseUrl}/suno/admissoes`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-access-token': process.env.ACCESS_TOKEN || '',
-        },
-    });
+class FindUsers {
+    public async execute(): Promise<IUser[]> {
+        const baseUrl = process.env.DOCLY_URL;
+        const response = await axios.get(`${baseUrl}/suno/admissoes`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': process.env.ACCESS_TOKEN || '',
+            },
+        });
 
-    return response.data;
-};
+        return response.data;
+    }
+}
 
-export { findUsers };
+export default FindUsers;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import dotenv from 'dotenv';
 import SendEmailService from '@services/SendEmailService';
-import { findUsers } from './repositories/findUsers';
+import FindUsers from '@repositories/findUsers';
+import Logging from '@config/winston';
 
 dotenv.config();
 (async (): Promise<void> => {
     const sendEmailService = new SendEmailService();
-    const users = await findUsers();
+    const findUsers = new FindUsers();
+
+    const users = await findUsers.execute();
+    if (!users.length) {
+        Logging.info('No users found');
+        return;
+    }
 
     users.forEach(async user => {
+        Logging.info(`Enviando email para ${user.email}`);
         await sendEmailService.execute(user);
     });
 })();
